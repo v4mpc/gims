ef13eb4095b58a7674c7a9a15e3615ef6e58a7df
Yona Mahundi <yona101992@gmail.com>
2024-08-03 19:12:40 +0300

    working on car make model

---
+++ console/src/pages/car/Columns.jsx
@@ -0,0 +1,26 @@
+const carColumns = [
+    {
+        title: "#",
+        dataIndex: "id",
+        key: "id",
+        width: "5%",
+    },
+    {
+        title: "Make",
+        key: "make",
+        dataIndex: "make",
+    },
+
+    {
+        title: "Model",
+        key: "model",
+        dataIndex: "model",
+    },
+
+    {
+        title: "Action",
+        key: "action",
+    },
+];
+
+export default carColumns;
