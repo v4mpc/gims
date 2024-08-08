15c020b3758989e00e440343440eeb4c2ba5e15f
Yona Mahundi <yona101992@gmail.com>
2024-08-08 14:42:08 +0300

    created paint repositories

---
+++ src/main/java/com/yhm/gims/repository/PaintLineItemRepository.java
@@ -0,0 +1,12 @@
+package com.yhm.gims.repository;
+
+import com.yhm.gims.entity.PaintLineItem;
+import com.yhm.gims.entity.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+
+public interface PaintLineItemRepository extends JpaRepository<PaintLineItem, Integer>{
+
+
+}
+
