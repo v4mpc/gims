23ed0034aa647895321c3041c651b276b3142305
Yona Mahundi <yona101992@gmail.com>
2025-04-27 01:39:20 +0300

    added standalone feature

---
+++ src-tauri/src/main.rs
@@ -0,0 +1,26 @@
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
+
+use std::process::{Command, Stdio};
+use std::thread;
+use std::time::Duration;
+
+fn main() {
+    // Start Spring Boot Backend
+    Command::new("java")
+        .args(["-jar", "bin/app.jar"])
+        .stdout(Stdio::null())
+        .stderr(Stdio::null())
+        .spawn()
+        .expect("Failed to start backend server");
+
+    // Wait a few seconds for Spring Boot to be ready
+    thread::sleep(Duration::from_secs(2));
+
+    tauri::Builder::default()
+        .invoke_handler(tauri::generate_handler![])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
